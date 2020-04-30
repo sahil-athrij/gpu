44a41b70b4a65954969e61c92c373b500033594b
sahil <sahilathrij@gmail.com>
2020-04-30 20:07:24 +0530

    initial commit

--- t_shftreg.vhdl
+++ t_shftreg.vhdl
@@ -11,6 +11,7 @@ architecture t_behaviour of t_pipo is
             en : in std_logic;
             D: in std_logic_vector(7 downto 0);
             Q: out std_logic_vector(7 downto 0)
+
         );
     end component pipo;
 
@@ -30,17 +31,35 @@ architecture t_behaviour of t_pipo is
        clk_process : process
         begin
             clk_t <= '0';
-            wait for 100 ms;
+            wait for 10 ms;
             clk_t <= '1';
-            wait for 100 ms;
+            wait for 10 ms;
+            if NOW > 500 ms then
+                wait;
+            end if;
+        end process;
+
+
+        en_process : process
+        begin
             en_t <= '1';
-            wait for 10 ms;
+            wait for 20 ms;
             en_t <= '0';
-            wait for 10 ms;
+            wait for 20 ms;
+            if NOW > 500 ms then
+                wait;
+            end if;
+        end process;
+
+        test_process : process
+        begin
             D_t <= "00001000";
-            wait for 20 ms;
+            wait for 40 ms;
             D_t <= "00001010";
-            wait for 20 ms;
+            wait for 40 ms;
+            if NOW > 500 ms then
+                wait;
+            end if;
         end process;
 
 
