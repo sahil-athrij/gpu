d41138690cf015fb50e0e29f920a4ce270330392
sahil <sahilathrij@gmail.com>
2020-05-01 14:28:22 +0530

    initial commit

--- shftreg.vhdl
+++ shftreg.vhdl
@@ -4,6 +4,7 @@ use ieee.std_logic_1164.all;
 entity pipo is
  port(
  clk : in std_logic;
+ op : in std_logic;
  en : in std_logic;
  D: in std_logic_vector(7 downto 0);
  Q: out std_logic_vector(7 downto 0)
@@ -15,10 +16,15 @@ architecture arch of pipo is
 begin
  
  process (clk,en)
+ signal temp   :  std_logic_vector(7 downto 0);
  begin
  if (CLK'event and CLK='1') then
     If (en ='1') then
-        Q <= D;
+        temp <= D;
+    end if;
+
+    If (op ='1') then
+        Q <= temp;
     end if;
  end if;
  end process;
