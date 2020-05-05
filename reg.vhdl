3345f586c7cf182f494c4edb8ad77a3011aca842
sha-ahammed <shahahamedroze@gmail.com>
2020-05-05 21:46:32 +0530

    further optimization

--- reg.vhdl
+++ reg.vhdl
@@ -6,27 +6,17 @@ entity reg is
     Port (
            -- 16 input / output buffer with one enable
            CLK  : in  std_logic;
-           IEN  : in  std_logic;
+           IEN  : in  std_logic; -- input enable; shift (0) rotate (1)
            INP  : in  std_logic_vector (15 downto 0);
-           OEN  : in  STD_LOGIC;
+           OEN  : in  STD_LOGIC; -- output enable; right(0) left (1)
            OUTP : out std_logic_vector (15 downto 0);
-           SOR  : IN STD_LOGIC; --shift (0) or rotate (1)
            mode : IN STD_LOGIC; -- shift rotate mode (1) or read/write mode (0)
-           RO   : IN STD_LOGIC; -- rotate right(0) left(1)
-           SH   : IN std_logic_vector(1 downto 0);
-           -- 00 arith right shift
-           -- 01 arith left shift
-           -- 10 logical right shift
-           -- 11 logical left shift
+           SH   : IN std_logic; -- arithmetic (0) or logical (1)
            );
 end reg;
 
 architecture Behavioral of reg is
-    signal temp : std_logic_vector (15 downto 0);
-    signal rs_shift : signed (15 downto 0);
-    signal ls_shift : signed (15 downto 0);
-    signal ru_shift : unsigned (15 downto 0);
-    signal lu_shift : unsigned (15 downto 0);
+
 begin
     process(CLK)
     begin
@@ -45,25 +35,28 @@ begin
             end if;
             
             if (mode = '1') then    -- shift/rotate mode
-                if (SOR = '0') then
-                    if (SH = '00') then
+                if (IEN = '0') then
+                    if (SH = '0') and (OEN = '0') then
                         temp <= shift_right (signed(temp), 1); -- arithmetic right shift
                     end if;
-                    if (SH = '01') then
+                    if (SH = '0') and (OEN = '1') then
                         temp <= shift_left (signed(temp), 1); -- arithmetic left shift
                     end if;        
-                    if (SH = '10') then
+                    if (SH = '1') and (OEN = '0') then
                         temp <= shift_right (unsigned(temp), 1); -- logical right shift
                     end if;
-                    if (SH = '11') then
+                    if (SH = '1') and (OEN = '1') then
                         temp <= shift_left (unsigned(temp), 1); -- logical left shift
-                    end if;  
-                elsif (SOR = '1') then
-                    if (RO = '0') then
+                    end if;
+                end if;      
+                if (IEN = '1') then
+                    if (OEN = '0') then
                         temp <= rotate_right (temp, 1);
                     end if;
-                    if (RO = '1') then
-                        temp <= rotate_left (temp, 1);    
+                    if (OEN = '1') then
+                        temp <= rotate_left (temp, 1);
+                    end if;
+                end if;            
             end if;        
                   
         end if;
