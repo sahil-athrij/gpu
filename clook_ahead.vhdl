4f1b87d1bcc59a937b74c9611c56f41de91fd40d
sahil <sahilathrij@gmail.com>
2020-05-11 03:33:43 +0530

    added shilftregister.vhdl and t_shiftregister.vhdl

--- clook_ahead.vhdl
+++ clook_ahead.vhdl
@@ -10,7 +10,7 @@ ENTITY c_l_addr IS
          sum       :  OUT  STD_LOGIC_VECTOR(15 DOWNTO 0);
          carry_out :  OUT  STD_LOGIC;
          aos       :  IN   STD_LOGIC;
-         en        :  IN   STD_LOGIC;   
+         en        :  IN   STD_LOGIC
         );
 END c_l_addr;
 
@@ -19,33 +19,40 @@ ARCHITECTURE behavioral OF c_l_addr IS
 SIGNAL    h_sum              :    STD_LOGIC_VECTOR(15 DOWNTO 0);
 SIGNAL    carry_generate     :    STD_LOGIC_VECTOR(15 DOWNTO 0);
 SIGNAL    carry_propagate    :    STD_LOGIC_VECTOR(15 DOWNTO 0);
+SIGNAL    not_yin           :    STD_LOGIC_VECTOR(15 DOWNTO 0);
+SIGNAL    not_carry_in      :    STD_LOGIC;
 SIGNAL    carry_in_internal  :    STD_LOGIC_VECTOR(15 DOWNTO 1);
 
 BEGIN
-    h_sum <= x_in XOR y_in;
-    carry_generate <= x_in AND y_in;
-    carry_propagate <= x_in OR y_in;
-    not_yin         <= NOT y_in;
+
     PROCESS (en, aos, carry_generate, carry_propagate, carry_in_internal)
     BEGIN
-    if (en = '1') then
+        h_sum <= x_in XOR y_in;
+        carry_generate <= x_in AND y_in;
+        carry_propagate <= x_in OR y_in;
+        not_yin         <= NOT y_in;
+         not_carry_in <=  carry_in ;
 
-        if (aos = '1') then
-            carry_in <= '1';
-            h_sum <= x_in XOR not_yin;
-            carry_generate <= x_in AND not_yin;
-            carry_propagate <= x_in OR not_yin;
+        if (en = '0') then
+            if (aos = '1') then
+                not_carry_in <= NOT carry_in;
+                h_sum <= x_in XOR not_yin;
+                carry_generate <= x_in AND not_yin;
+                carry_propagate <= x_in OR not_yin;
+            end if;
+
+            carry_in_internal(1) <= carry_generate(0) OR (carry_propagate(0) AND carry_in);
+            inst: FOR i IN 1 TO 14 LOOP
+                  carry_in_internal(i+1) <= carry_generate(i) OR (carry_propagate(i) AND carry_in_internal(i));
+                  END LOOP;
+            carry_out <= carry_generate(15) OR (carry_propagate(15) AND carry_in_internal(15));
+
+
+        sum(0) <= h_sum(0) XOR not_carry_in;
+        sum(15 DOWNTO 1) <= h_sum(15 DOWNTO 1) XOR carry_in_internal(15 DOWNTO 1);
+
+        else
+            sum<=(others=>'Z');
         end if;
-        
-        carry_in_internal(1) <= carry_generate(0) OR (carry_propagate(0) AND carry_in);
-        inst: FOR i IN 1 TO 14 LOOP
-              carry_in_internal(i+1) <= carry_generate(i) OR (carry_propagate(i) AND carry_in_internal(i));
-              END LOOP;
-        carry_out <= carry_generate(15) OR (carry_propagate(15) AND carry_in_internal(15));
     END PROCESS;
-
-    sum(0) <= h_sum(0) XOR carry_in;
-    sum(15 DOWNTO 1) <= h_sum(15 DOWNTO 1) XOR carry_in_internal(15 DOWNTO 1);
-
-    end if;    
 END behavioral;
---
+++ t_clook_ahead.vhdl
@@ -0,0 +1,58 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity t_c_l_addr is
+end t_c_l_addr;
+
+architecture t_behaviour of t_c_l_addr is
+    component c_l_addr is
+        port(
+             x_in      :  IN   STD_LOGIC_VECTOR(15 DOWNTO 0);
+             y_in      :  IN   STD_LOGIC_VECTOR(15 DOWNTO 0);
+             carry_in  :  IN   STD_LOGIC;
+             sum       :  OUT  STD_LOGIC_VECTOR(15 DOWNTO 0);
+             carry_out :  OUT  STD_LOGIC;
+             aos       :  IN   STD_LOGIC;
+             en        :  IN   STD_LOGIC
+
+        );
+    end component c_l_addr;
+    signal x_in_t      :    STD_LOGIC_VECTOR(15 DOWNTO 0);
+    signal y_in_t      :    STD_LOGIC_VECTOR(15 DOWNTO 0);
+    signal carry_in_t  :    STD_LOGIC;
+    signal sum_t       :    STD_LOGIC_VECTOR(15 DOWNTO 0);
+    signal carry_out_t :    STD_LOGIC;
+    signal aos_t       :    STD_LOGIC;
+    signal en_t        :    STD_LOGIC;
+    begin
+        module_reg: c_l_addr
+        port map(
+            x_in => x_in_t,
+            y_in  => y_in_t,
+            carry_in  => carry_in_t,
+            sum  => sum_t,
+            carry_out  => carry_out_t,
+            aos   => aos_t,
+            en   => en_t
+        );
+        en_process : process
+        begin
+            en_t <='0';
+            aos_t <='0';
+            carry_in_t <='0';
+            x_in_t <="1111000011110000";
+            y_in_t <="0000111100001111";
+            wait for 20 ms;
+            x_in_t <="0000000011110000";
+            y_in_t <="0000000011100000";
+            wait for 20 ms;
+            aos_t <='1';
+            wait for 20 ms;
+            en_t <='1';
+            wait;
+        end process;
+
+
+
+end t_behaviour;
+
