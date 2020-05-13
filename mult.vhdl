f259479a4b907ae068f6726682929fef2a6b3ca0
sha-ahammed <shahahamedroze@gmail.com>
2020-05-14 03:09:50 +0530

    multiplier 2.0

--- mult.vhdl
+++ mult.vhdl
@@ -4,15 +4,22 @@ use ieee.numeric_std.all;
 entity mult is
 
     port (
-        a,b : in  signed(15 downto 0);
-        c   : out signed(31 downto 0)
+        a,b : in  std_logic_vector(15 downto 0);
+        c   : out std_logic_vector(31 downto 0);
+        s   : in std_logic;
+
     );
 
 end mult;
 
 architecture rtl of mult is
 begin
-  
-    c <= a * b;
-  
+    if (s = '0') then
+        c <= unsigned(a) * unsigned(b);
+    elsif (s = '1') then
+        c <= signed(a) * signed(b);     
+    else
+        c <= (others => 'Z');
+    end if;        
+    
 end architecture rtl;
--- t_mult.vhdl
+++ t_mult.vhdl
@@ -7,31 +7,48 @@ end t_mult;
 architecture t_rtl of t_mult is
     component mult is
         port (
-            a,b : in  signed(15 downto 0);
-            c   : out signed(31 downto 0)
+            a,b : in  std_logic_vector(15 downto 0);
+            c   : out std_logic_vector(31 downto 0);
+            s   : in std_logic;
+    
         );
+        
     end component mult;
     
-    signal a_t : signed(15 downto 0);
-    signal b_t : signed(15 downto 0);
-    signal c_t : signed(31 downto 0);
+    signal a_t : std_logic_vector(15 downto 0);
+    signal b_t : std_logic_vector(15 downto 0);
+    signal c_t : std_logic_vector(31 downto 0);
+    signal s_t : std_logic;
 
     begin
         module_mult: MULT
         port map (
             a => a_t,
             b => b_t,
-            c => c_t
+            c => c_t,
+            s => s_t
         );
 
         test_process : process
         begin
             a_t <= "1111000011110000";
             b_t <= "0000111100001111";
+            s_t <= "0";
             wait for 20 ms;
 
             a_t <= "0000000011100000";
             b_t <= "0000000011110000";
+            s_t <= "0";
+            wait for 20 ms;
+
+            a_t <= "1111000011110000";
+            b_t <= "0000111100001111";
+            s_t <= "1";
+            wait for 20 ms;
+
+            a_t <= "0000000011100000";
+            b_t <= "0000000011110000";
+            s_t <= "1";
             wait for 20 ms;
 
             wait;
