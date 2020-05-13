d93a7e4562af94e9f08dcae1e6626e7c7216ad9e
sha-ahammed <shahahamedroze@gmail.com>
2020-05-14 05:10:51 +0530

    added divide

--- mult.vhdl
+++ mult.vhdl
@@ -6,7 +6,7 @@ entity mult is
     port (
         a,b : in  std_logic_vector(15 downto 0);
         c   : out std_logic_vector(31 downto 0);
-        s   : in std_logic
+        s   : in  std_logic_vector( 1 downto 0);
 
     );
 
@@ -17,11 +17,14 @@ architecture rtl of mult is
 begin
 PROCESS ( a,b,s)
             begin
-        if (s = '0') then
+        if (s = '00') then
             c <= std_logic_vector(unsigned(a) * unsigned(b));
-        elsif (s = '1') then
-
+        elsif (s = '01') then
             c <= std_logic_vector(signed(a) * signed(b));
+        elsif (s = '10') then
+            c <= std_logic_vector(unsigned(a) / unsigned(b));
+        elsif (s = '11') then
+            c <= std_logic_vector(signed(a) / signed(b));        
         else
             c <= (others => 'Z');
         end if;
